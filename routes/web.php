63a95281b243d0810d31591605c6f6a5a52ccab4
John Lyold Lozada <167066974+DeJure12705@users.noreply.github.com>
2024-11-19 13:15:04 +0800

    added routes on product store

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,7 @@ Route::middleware(['auth', AuthAdmin::class])->group(function () {
     Route::post('/admin/category/update', [AdminController::class, 'category_update'])->name('admin.category.update');
     Route::delete('/admin/category/{id}/delete', [AdminController::class, 'category_delete'])->name('admin.category.delete');
     Route::get('/admin/products', [AdminController::class, 'products'])->name('admin.products');
+    Route::get('/admin/product/add', [AdminController::class, 'product_add'])->name('admin.product.add');
+    Route::post('/admin/product/store', [AdminController::class, 'product_store'])->name('admin.product.store');
+    Route::get('/admin/product/{id}/edit', [AdminController::class, 'product_edit'])->name('admin.product.edit');
 });
