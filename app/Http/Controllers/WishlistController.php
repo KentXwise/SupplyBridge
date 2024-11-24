34e882171a2447a4388ce38d203b249daad0f5a7
KentXwise <geraldo.kentdiether@dnsc.edu.ph>
2024-11-25 00:06:21 +0800

    Laravel 27 and 28

---
+++ app/Http/Controllers/WishlistController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Surfsidemedia\Shoppingcart\Facades\Cart; // Ensure this import is correct
+use App\Models\Product; // Import Product model
+
+class WishlistController extends Controller
+{
+    public function index()
+    {
+        $items = Cart::instance('wishlist')->content();
+        return view('wishlist', compact('items'));
+    }
+    public function add_to_wishlist(Request $request)
+    {
+        Cart::instance('wishlist')->add(
+            $request->id,
+            $request->name, 
+            $request->quantity,
+            $request->price
+        )->associate(Product::class);
+
+        return redirect()->back();
+    }
+}
