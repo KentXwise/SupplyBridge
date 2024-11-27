ca4c4ad957c5ba8339dd7d2d0b4b57468ff9726b
John Lyold Lozada <167066974+DeJure12705@users.noreply.github.com>
2024-11-27 13:28:53 +0800

    Update admin.blade

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -116,23 +116,11 @@
               </li>
        </ul>
 </li>    
-                                <li class="menu-item has-children">
-                                    <a href="javascript:void(0);" class="menu-item-button">
+                                <li class="menu-item">
+                                    <a href="{{route('admin.orders')}}" class="menu-item-button">
                                         <div class="icon"><i class="icon-file-plus"></i></div>
                                         <div class="text">Order</div>
                                     </a>
-                                    <ul class="sub-menu">
-                                        <li class="sub-menu-item">
-                                            <a href="{{route('admin.orders')}}" class="">
-                                                <div class="text">Orders</div>
-                                            </a>
-                                        </li>
-                                        <li class="sub-menu-item">
-                                            <a href="order-tracking.html" class="">
-                                                <div class="text">Order tracking</div>
-                                            </a>
-                                        </li>
-                                    </ul>
                                 </li>
                                 <li class="menu-item">
                                     <a href="{{route('admin.contacts')}}" class="">
