71e5c9fb26772f8022b7e7a10da2fc16a5c606c9
ChristInsanity <xian23xian23xian@gmail.com>
2024-11-29 00:24:38 +0800

    user-

--- resources/views/user/account-nav.blade.php
+++ resources/views/user/account-nav.blade.php
@@ -1,7 +1,7 @@
 <ul class="account-nav">
-            <li><a href="{{route('user.index')}}" class="menu-link menu-link_us-s">Dashboard</a></li>
+            <li><a href="{{route('cart.index')}}" class="menu-link menu-link_us-s">Cart</a></li>
             <li><a href="{{route('user.orders')}}" class="menu-link menu-link_us-s">Orders</a></li>
-            <li><a href="account-wishlist.html" class="menu-link menu-link_us-s">Wishlist</a></li>
+            <li><a href="{{route('wishlist.index')}}" class="menu-link menu-link_us-s">Wishlist</a></li>
 
             <li>
                 <form method="POST" action="{{route('logout')}}" id="logout-form">
